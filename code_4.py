04ef6430fdb9aba8c056587065b81c4fed41ed34
eiflores2 <eiflores2@miners.utep.edu>
2024-09-26 10:18:03 -0600

    Input a query for the first def dunction

--- code_4.py
+++ code_4.py
@@ -1,9 +1,11 @@
-import csv
 import pandas as pd
 df = pd.read_csv('./big-mac-full-index.csv')
 
 def get_big_mac_price_by_year(year,country_code):
-    pass # Remove this line and code your function
+    query = f"(date == '{year}' iso_a3 == '{country_code}')"
+    sub_df = df.query(query)
+    print(sub_df)
+
 
 def get_big_mac_price_by_country(country_code):
     pass # Remove this line and code your function
@@ -15,4 +17,4 @@ def get_the_most_expensive_big_mac_price_by_year(year):
     pass # Remove this line and code your function
 
 if __name__ == "__main__":
-    pass # Remove this line and code your user interface
+    pass 
