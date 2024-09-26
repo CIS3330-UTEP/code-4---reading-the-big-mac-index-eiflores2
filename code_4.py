82ae9e4f50482e62a5ad213bc06d4481ddb5e983
eiflores2 <eiflores2@miners.utep.edu>
2024-09-26 10:41:36 -0600

    Added mean to the first function

--- code_4.py
+++ code_4.py
@@ -2,9 +2,10 @@ import pandas as pd
 df = pd.read_csv('./big-mac-full-index.csv')
 
 def get_big_mac_price_by_year(year,country_code):
-    query = f"(date == '{year}' iso_a3 == '{country_code}')"
+    query = f"(date == '{year}' & iso_a3 == '{country_code.lower()}')"
     sub_df = df.query(query)
-    print(sub_df)
+    mean_price =sub_df['dollar_price'].mean()
+    return round(mean_price, 2)
 
 
 def get_big_mac_price_by_country(country_code):
